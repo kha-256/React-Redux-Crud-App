bd4d7c9ac0e71a461d1ded948ef1a9f27f8de70d
khadija <khadijanoorhashmi@gmail.com>
2023-05-28 18:28:14 +0500

    changes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import UserInformation from './pages/UserInformation'
+
+function App() {
+  
+
+  return (
+    <>
+      <UserInformation/>
+
+       </>
+  )
+}
+
+export default App
